9389995837d4f13cdb2bccd9ac27131b5ab2df01
qwsq4 <qwsq1437@gmail.com>
2022-09-13 13:13:17 +0800

    Сделано ДЗ

--- src/Main.java
+++ src/Main.java
@@ -1,84 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        //Домашнее задание 1
-        //Задача 1
-        System.out.println("   ДЗ №1. Задание 1");
-        int age = 13;
-        if (age >= 18) {
-            System.out.println("Поздравляю с совершеннолетием!");
-        }
-        if (age < 18) {
-            System.out.println("Возраст совершеннолетия еще не достигнут. Нужно подождать " + (18 - age) + " лет");
-        }
-
-        //Задача 2
-        System.out.println("   ДЗ №1. Задание 2");
-        if (age >= 7) {
-            System.out.println("Ребёнок ходит в школу");
-        }
-        if (age >= 18) {
-            System.out.println("Молодой человек окончил школу и может продолжить обучение в университете");
-        }
-        if (age >= 24) {
-            System.out.println("Человек окончил университет и ему пора искать первую работу");
-        }
-
-        //Задача 3
-        System.out.println("   ДЗ №1. Задание 3");
-        int trainCarCapacity = 102;
-        int seatCount = 60;
-        int passengers = 500;
-        if (passengers >= trainCarCapacity) {
-            System.out.println("Мест в вагоне нет");
-        }
-        if (passengers < trainCarCapacity) {
-            System.out.println("В вагоне есть " + (trainCarCapacity - passengers) + " мест");
-            if (passengers >= seatCount) {
-                System.out.println("Сидячих мест в вагоне нет");
-            }
-            if (passengers < seatCount) {
-                System.out.println("В вагоне " + (seatCount - passengers) + " сидячих мест");
-            }
-        }
-
-        //Домашнее задание 2
-        //Задача 1
-        System.out.println("   ДЗ №2. Задание 1");
-        if (age >= 18) {
-            System.out.println("Поздравляю с совершеннолетием!");
-        } else {
-            System.out.println("Возраст совершеннолетия еще не достигнут. Нужно подождать " + (18 - age) + " лет");
-        }
-
-        //Задача 2
-        System.out.println("   ДЗ №2. Задание 2");
-        if (age >= 24) {
-            System.out.println("Человек окончил университет и ему пора искать первую работу");
-        } else {
-            if (age >= 18) {
-                System.out.println("Молодой человек окончил школу и может продолжить обучение в университете");
-            } else {
-                if (age >= 7) {
-                    System.out.println("Ребёнок ходит в школу");
-                }
-            }
-        }
-
-        //Задача 3
-        System.out.println("   ДЗ №2. Задание 3");
-        if (passengers >= trainCarCapacity) {
-            System.out.println("Мест в вагоне нет");
-        }else{
-            System.out.println("В вагоне есть " + (trainCarCapacity - passengers) + " мест");
-            if (passengers >= seatCount) {
-                System.out.println("Сидячих мест в вагоне нет");
-            }else {
-                System.out.println("В вагоне " + (seatCount - passengers) + " сидячих мест");
-            }
-        }
-
         //Домашнее задание 3
         //Задача 1
+        int age = 12;
         System.out.println("   ДЗ №3. Задание 1");
         boolean needGoToKindergarten = age >= 2 && age <=6;
         boolean needGoToSchool = age >= 7 && age <=18;
